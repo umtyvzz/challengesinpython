b94ce6d30fbc8901a5e995b6d621269ae83ad4f1
umtyvzz <umtyvz.35@hotmail.com>
2022-09-17 01:04:39 -0800

    Rock Paper Scissors Game Added.

--- pythonday4.py
+++ pythonday4.py
@@ -1,3 +1,74 @@
-import random   
-x = random.randint(1, 10)
-print(x)
+# import random 
+# # import my_module
+# # print(my_module.pi)
+  
+# x = random.randint(1, 10)
+# print(x)
+
+# y = random.random()
+# a = y * 100
+# print(a)
+
+print("Welcome to Rock Paper Scissors Game")
+
+import random
+ask = int(input("Make your choise ~ 1(Rock) 2(Paper) 3(Scissors)"))
+if ask == 1:
+    print("""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""")
+    
+if ask == 2:
+    print("""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""")
+
+if ask == 3:
+    print("""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""")
+x = random.randint(0, 2)
+
+if x == 0:
+    print("""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""")
+elif x == 1:
+    print("""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""")
+
+else:
+    print("""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""")
