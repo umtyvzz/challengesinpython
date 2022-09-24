8c60e403fdebf6e45509f77a2c92a0b2a826abc8
umtyvzz <umtyvz.35@hotmail.com>
2022-09-23 23:10:47 -0800

    ccsscc

---
+++ pythonday4ex2.py
@@ -0,0 +1,9 @@
+print("Welcome to the game Who is paying?")
+
+# import random
+
+names = input("Give me everyone's names, seperated by a comma.")
+
+names = names_string.split(", ")
+
+print(names)
